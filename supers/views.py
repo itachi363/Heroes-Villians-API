62e4af0fc10d66bcde357f887aed5cd1174524d3
itachi363 <itachi363@yahoo.com>
2022-03-18 12:05:49 -0400

    got sorting of hearoes and villians done in views but still working out how to get it all to work together

--- supers/views.py
+++ supers/views.py
@@ -4,18 +4,34 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
+from super_types.models import Super_Type
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
+    supers = Super.objects.all()
+    types_super = Super_Type.objects.all()
 
+
+    appending_dict = {}
+    appending_dict['type'] = ''
+    print(appending_dict)
+
+    custom_response_dictionary = {}
+
+    for type_super in types_super:
+        super_type_id = Super.objects.filter(super_type=type_super.id)
+        super_serializer = SuperSerializer(super_type_id, many=True)
+        custom_response_dictionary[type_super.type] = {
+            "Supers": super_serializer.data
+        }
+    
+    return Response(custom_response_dictionary)
     if request.method == 'GET':
         type_of_super = request.query_params.get('type')
-        print(type_of_super)
-        supers = Super.objects.all()
 
         if type_of_super:
             supers = supers.filter(super_type__type=type_of_super)
-            
+
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
 
@@ -24,6 +40,7 @@ def supers_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
